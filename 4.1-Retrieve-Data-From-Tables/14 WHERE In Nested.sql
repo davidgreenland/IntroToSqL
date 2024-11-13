1a8a43008bb122d991008d4e225ae8a06d7fdf11
David Greenland <david.greenland@brighthr.com>
2024-11-13 16:51:29 +0000

    Update 14 WHERE In Nested.sql

--- 4.1-Retrieve-Data-From-Tables/14 WHERE In Nested.sql
+++ 4.1-Retrieve-Data-From-Tables/14 WHERE In Nested.sql
@@ -1,7 +1,8 @@
 -- Events that have a duplicate EventDate
 
 SELECT * FROM Event
-WHERE EventDate IN (SELECT EventDate
+WHERE EventDate IN 
+  (SELECT EventDate
 FROM Event
 GROUP BY EventDate
 HAVING COUNT(*) > 1)
